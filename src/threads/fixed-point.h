ea5c08c950209d4cdf525f95df34e377e4078e31
Andrew Hume <andrew.hume15@imperial.ac.uk>
2017-01-27 14:25:01 +0000

    [AH] surrounded all macro arguments with brackets for safety

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -8,22 +8,24 @@ typedef int32_t fixed_point;
 #define FRACTIONAL_BITS 14
 #define FIXED_POINT_FACTOR (1 << FRACTIONAL_BITS)
 
-#define INT_TO_FIXED_POINT(n) (n * FIXED_POINT_FACTOR)
+#define INT_TO_FIXED_POINT(n) ((n) * FIXED_POINT_FACTOR)
 
-#define TO_INT_ROUND_0(x) (x / FIXED_POINT_FACTOR)
-#define TO_INT_ROUND_NEAREST(x) (x >= 0 ? \
-    (x + FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR : \
-    (x + FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR)
+#define TO_INT_ROUND_0(x) ((x) / FIXED_POINT_FACTOR)
+#define TO_INT_ROUND_NEAREST(x) ((x) >= 0 ? \
+    ((x) + FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR : \
+    ((x) + FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR)
 
-#define ADD_FIXED_POINT_FIXED_POINT(x, y) (x + y)
-#define SUB_FIXED_POINT_FIXED_POINT(x, y) (x - y)
-#define ADD_FIXED_POINT_INT(x, y) (x + INT_TO_FIXED_POINT(y))
-#define SUB_FIXED_POINT_INT(x, y) (x - INT_TO_FIXED_POINT(y))
-#define SUB_INT_FIXED_POINT(x, y) (INT_TO_FIXED_POINT(x) - y)
+#define ADD_FIXED_POINT_FIXED_POINT(x, y) ((x) + (y))
+#define SUB_FIXED_POINT_FIXED_POINT(x, y) ((x) - (y))
+#define ADD_FIXED_POINT_INT(x, y) ((x) + INT_TO_FIXED_POINT(y))
+#define SUB_FIXED_POINT_INT(x, y) ((x) - INT_TO_FIXED_POINT(y))
+#define SUB_INT_FIXED_POINT(x, y) (INT_TO_FIXED_POINT(x) - (y))
 
-#define MUL_FIXED_POINT_FIXED_POINT(x, y) (((int64_t) x) * y / f)
-#define MUL_FIXED_POINT_INT(x, n) (x * n)
-#define DIV_FIXED_POINT_FIXED_POINT(x, y) (((int64_t) x) * f / y)
-#define DIV_FIXED_POINT_INT(x, n) (x / n)
+#define MUL_FIXED_POINT_FIXED_POINT(x, y) \
+  (((int64_t) (x)) * (y) / FIXED_POINT_FACTOR)
+#define MUL_FIXED_POINT_INT(x, n) ((x) * (n))
+#define DIV_FIXED_POINT_FIXED_POINT(x, y) \
+ (((int64_t) (x)) * FIXED_POINT_FACTOR / (y))
+#define DIV_FIXED_POINT_INT(x, n) ((x) / (n))
 
 #endif /* threads/fixed-point.h */
