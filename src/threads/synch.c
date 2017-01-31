b1111d77092430513f51bbf34cfd2f5a21c7d9d0
Tanmay <tk915@ic.ac.uk>
2017-01-31 16:56:31 +0000

    [TK][AB] Fixed bug with semaphore waiters not being updated when there's a priority donation.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -130,9 +130,11 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
+  if (!list_empty (&sema->waiters)) {
+    list_sort(&sema->waiters, higher_priority, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
+  }
   sema->value++;
   if (!intr_context()) {
     yield_if_higher_priority_ready();
