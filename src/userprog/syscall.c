e4edd2c8cd65194cbfad7b230f0af41c1f83d7d9
Tanmay Khanna <tk915@ray09.doc.ic.ac.uk>
2017-02-13 14:59:29 +0000

    [TK] Added stub code for system calls. Also fixed issue with warnings about incompatible pointer types in system_calls.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -21,7 +21,7 @@ static void sys_seek (struct intr_frame *);
 static void sys_tell (struct intr_frame *);
 static void sys_close (struct intr_frame *);
 
-static const void (*system_calls[]) (struct intr_frame *f) = {
+static void (*system_calls[]) (struct intr_frame *) = {
   &sys_halt, &sys_exit, &sys_exec, &sys_wait, &sys_create, &sys_remove,
   &sys_open, &sys_filesize, &sys_read, &sys_write, &sys_seek, &sys_tell,
   &sys_close
@@ -35,8 +35,61 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
   thread_exit ();
 }
+
+static void sys_halt (struct intr_frame * f UNUSED) {
+  shutdown_power_off();
+}
+
+static void sys_exit (struct intr_frame * f) {
+
+}
+
+static void sys_exec (struct intr_frame * f) {
+
+}
+
+static void sys_wait (struct intr_frame * f) {
+
+}
+
+static void sys_create (struct intr_frame * f) {
+
+}
+
+static void sys_remove (struct intr_frame * f){
+
+}
+
+static void sys_open (struct intr_frame * f){
+
+}
+
+static void sys_filesize (struct intr_frame * f){
+
+}
+
+static void sys_read (struct intr_frame * f){
+
+}
+
+static void sys_write (struct intr_frame * f){
+
+}
+
+static void sys_seek (struct intr_frame * f){
+
+}
+
+static void sys_tell (struct intr_frame * f){
+
+}
+
+static void sys_close (struct intr_frame * f){
+
+}
+
