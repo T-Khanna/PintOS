d33bdad9d984deb5728add1bf3eb4a891c0d3145
Tanmay Khanna <tk915@ray09.doc.ic.ac.uk>
2017-02-13 14:42:38 +0000

    [TK] Created function pointer table to hold pointers to system calls.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,13 +3,35 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
+
+static void sys_halt (struct intr_frame *);
+static void sys_exit (struct intr_frame *);
+static void sys_exec (struct intr_frame *);
+static void sys_wait (struct intr_frame *);
+static void sys_create (struct intr_frame *);
+static void sys_remove (struct intr_frame *);
+static void sys_open (struct intr_frame *);
+static void sys_filesize (struct intr_frame *);
+static void sys_read (struct intr_frame *);
+static void sys_write (struct intr_frame *);
+static void sys_seek (struct intr_frame *);
+static void sys_tell (struct intr_frame *);
+static void sys_close (struct intr_frame *);
+
+static const void system_calls[] (struct intr_frame *f) = {
+  &sys_halt, &sys_exit, &sys_exec, &sys_wait, &sys_create, &sys_remove,
+  &sys_open, &sys_filesize, &sys_read, &sys_write, &sys_seek, &sys_tell,
+  &sys_close
+}
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  thread_exit ();
 }
 
 static void
