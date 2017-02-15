20c01b69d61a0f791634801d53a9977c9b052d27
Tanmay <tk915@ic.ac.uk>
2017-02-15 16:13:25 +0000

    [TK] Added basic system call handler to call the right system calls.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,9 +1,11 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <user/syscall.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -31,14 +33,16 @@ void
 syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-  thread_exit ();
+  // thread_exit ();
 }
 
 static void
 syscall_handler (struct intr_frame *f)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int32_t syscall_number = (int32_t) f->esp;
+  system_calls[syscall_number](f);
+  // printf ("system call!\n");
+  // thread_exit ();
 }
 
 void* get_arg(struct intr_frame* f, int arg_num) {
@@ -61,7 +65,9 @@ static void sys_exit (struct intr_frame * f) {
 }
 
 static void sys_exec (struct intr_frame * f) {
-
+  const char* cmd_line = (const char*) get_arg(f, 1);
+  pid_t pid = process_execute(cmd_line);
+  f->eax = pid;
 }
 
 static void sys_wait (struct intr_frame * f) {
@@ -89,7 +95,13 @@ static void sys_read (struct intr_frame * f) {
 
 }
 static void sys_write (struct intr_frame * f) {
-
+  int fd = (int) get_arg(f, 1);
+  const void* buffer = (const void*) get_arg(f, 2);
+  unsigned size = (unsigned) get_arg(f, 3);
+  int bytes_written = 0;
+  /*TODO: Need to check for invalid pointers (user memory access) and also
+   *      keep track of the number of bytes written to console. */
+  f->eax = bytes_written;
 }
 
 static void sys_seek (struct intr_frame * f) {
