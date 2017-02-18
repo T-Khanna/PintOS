aa33b195a1b791f9b513c6dd413ead976d2d872c
Alessandro Bonardi <ab9515@point08.doc.ic.ac.uk>
2017-02-18 21:13:31 +0000

    [AB] Started adding ponters checking in system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,7 @@
 #include "threads/thread.h"
 #include "devices/shutdown.h"
 #include "userprog/process.h"
+#include "userprog/pagedir.h"
 #include "filesys/file.h"
 #include "filesys/filesys.h"
 
@@ -32,6 +33,8 @@ static void sys_seek (struct intr_frame *);
 static void sys_tell (struct intr_frame *);
 static void sys_close (struct intr_frame *);
 
+static bool check_safe_access(void *ptr, unsigned size);
+
 /* contiguous number of system calls implemented, starting from 0 (HALT) */
 #define IMPLEMENTED_SYSCALLS 12
 
@@ -55,6 +58,7 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f)
 {
+  //hex_dump(0, f->esp, 0xc0000000 - (int) f->esp, 1);    
   int32_t syscall_number = *(int32_t *) f->esp;
   //kill thread if the system call number is bad.
   if (syscall_number < 0 || syscall_number > IMPLEMENTED_SYSCALLS) {
@@ -63,12 +67,14 @@ syscall_handler (struct intr_frame *f)
   system_calls[syscall_number](f);
 }
 
-void* get_arg(struct intr_frame* f, int arg_num) {
+void* get_arg(struct intr_frame* f, int arg_num)
+{
   return  *((int32_t *) f->esp + arg_num);
 }
 
 
-static void sys_halt (struct intr_frame * f UNUSED) {
+static void sys_halt (struct intr_frame * f UNUSED)
+{
   shutdown_power_off();
   NOT_REACHED();
 }
@@ -79,13 +85,15 @@ void exit(int status) {
   NOT_REACHED();
 }
 
-static void sys_exit (struct intr_frame * f) {
+static void sys_exit (struct intr_frame * f)
+{
   // This may not be entirely finished. TODO
   int status = (int) get_arg(f, 1);
   exit(status);
 }
 
-static void sys_exec (struct intr_frame * f) {
+static void sys_exec (struct intr_frame * f)
+{
   /*TODO: Need to make sure this is synchronized properly to force the parent
    *      process to wait until the child process has successfully loaded. */
   const char* cmd_line = (const char*) get_arg(f, 1);
@@ -93,12 +101,14 @@ static void sys_exec (struct intr_frame * f) {
   f->eax = pid;
 }
 
-static void sys_wait (struct intr_frame * f) {
+static void sys_wait (struct intr_frame * f)
+{
   pid_t pid = (pid_t) get_arg(f, 1);
   f->eax = process_wait(pid);
 }
 
-static void sys_create (struct intr_frame * f) {
+static void sys_create (struct intr_frame * f)
+{
   const char* file = (const char*) get_arg(f, 1);
   unsigned initial_size = (unsigned) get_arg(f, 2);
 
@@ -109,7 +119,8 @@ static void sys_create (struct intr_frame * f) {
   f->eax = success;
 }
 
-static void sys_remove (struct intr_frame * f) {
+static void sys_remove(struct intr_frame * f)
+{
   const char* file = (const char*) get_arg(f, 1);
 
   lock_acquire(&filesys_lock);
@@ -119,9 +130,20 @@ static void sys_remove (struct intr_frame * f) {
   f->eax = success;
 }
 
-static void sys_open (struct intr_frame * f) {
+static void sys_open(struct intr_frame * f)
+{
+  
+  printf("The address is %p\n", get_arg(f, 1));
+  
+  int fd = -1; // File descriptor/ -1 if the file could not be opened.
+
+  if (!check_safe_access(get_arg(f, 1), 1)) {
+    printf("The file cannot be open\n");
+    f->eax = fd;
+    return;
+  }
+    
   const char* name = (const char*) get_arg(f, 1);
-  int fd = -1; // File descriptor. -1 if the file could not be opened.
 
   lock_acquire(&filesys_lock);
   struct file* file = filesys_open(name);
@@ -129,14 +151,17 @@ static void sys_open (struct intr_frame * f) {
     struct descriptor desc;
     desc.id = fd_count++;
     desc.file = file;
+    fd = desc.id;
     list_push_front (&thread_current()->descriptors, &desc.elem);
   }
   lock_release(&filesys_lock);
 
   f->eax = fd;
+
 }
 
-static void sys_filesize (struct intr_frame * f) {
+static void sys_filesize(struct intr_frame * f)
+{
   int fd = (int) get_arg(f, 1);
   int file_byte_size = 0;
 
@@ -145,7 +170,8 @@ static void sys_filesize (struct intr_frame * f) {
   f->eax = file_byte_size;
 }
 
-static void sys_read (struct intr_frame * f) {
+static void sys_read(struct intr_frame * f)
+{
   int fd = (int) get_arg(f, 1);
   void* buffer = get_arg(f, 2);
   unsigned size = (unsigned) get_arg(f, 3);
@@ -154,10 +180,13 @@ static void sys_read (struct intr_frame * f) {
   f->eax = bytes_read;
 }
 
-static void sys_write (struct intr_frame * f) {
+static void sys_write(struct intr_frame * f)
+{
   int fd = (int) get_arg(f, 1);
   const void* buffer = (const void*) get_arg(f, 2);
   unsigned size = (unsigned) get_arg(f, 3);
+    //printf("I AM INSIDE WRITE! THE SIZE IS %d\n", size);
+  
 
   int bytes_written = 0;
   /*TODO: Need to check for invalid pointers (user memory access) and also
@@ -170,20 +199,46 @@ static void sys_write (struct intr_frame * f) {
   f->eax = bytes_written;
 }
 
-static void sys_seek (struct intr_frame * f) {
+static void sys_seek(struct intr_frame * f)
+{
   int fd = (int) get_arg(f, 1);
   unsigned position = (unsigned) get_arg(f, 2);
   //TODO
 }
 
-static void sys_tell (struct intr_frame * f) {
+static void sys_tell(struct intr_frame * f)
+{
   int fd = (int) get_arg(f, 1);
   unsigned position = 0;
   //TODO
   f->eax = position;
 }
 
-static void sys_close (struct intr_frame * f) {
+static void sys_close(struct intr_frame * f)
+{
   int fd = (int) get_arg(f, 1);
   //TODO
 }
+
+
+
+static bool check_safe_access(void *ptr, unsigned size)
+{
+  /* Checks that the pointer is not null */
+  if (ptr == NULL) {
+    return false;
+  }
+
+  for (int i = 0; i < size; i++) {
+    if (pagedir_get_page(thread_current()->pagedir, 
+                (char *) ptr + i) == NULL) {
+        return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+
