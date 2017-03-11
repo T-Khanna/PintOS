009336d0673c0011f96e0a74f52727f1e0e8d2d9
Alessandro Bonardi <ab9515@point53.doc.ic.ac.uk>
2017-03-11 16:35:43 +0000

    [AB] Added basic elements to frame struct

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,7 +1,13 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
+#include "userprog/process.h"
+
 struct frame {
+
+    struct process *process;    /* Reference to the process it belongs to */
+    void *page_addr;            /* Reference to page address in kernel vm */
+    struct hash_elem hash_elem; /* Hash table element */
 
 };
 
