694f00775c7a7b4386b23222657e2bf6cba18dea
Andrew Hume <andrew.hume15@imperial.ac.uk>
2017-03-11 15:39:54 +0000

    [AH] swap slot bitmap and functions to setup and destroy a thread's swap table

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,12 +1,60 @@
 #include "vm/swap.h"
+#include <bitmap.h>
 #include "devices/block.h"
 #include "threads/vaddr.h"
 
-static struct block *swap_dev;  /* The swap device */
-static const int sectors_per_page = PGSIZE / BLOCK_SECTOR_SIZE;
-static int num_slots;           /* Total number of swap slots */
+/* Functions required to use hash table */
+unsigned swap_table_hash_func(const struct hash_elem *elem, void *aux UNUSED);
+bool swap_table_less_func(const struct hash_elem *a, const struct hash_elem *b,
+    void *aux UNUSED);
 
-void swap_init(void) {
+static struct block *swap_dev;     /* The swap device */
+static const int sectors_per_page = PGSIZE / BLOCK_SECTOR_SIZE;
+static size_t num_slots;           /* Total number of swap slots */
+static struct bitmap *slot_usage;  /* Bitmap to represent usage of slots */
+
+/* Initialise the swap system */
+void swap_init(void)
+{
   swap_dev = block_get_role(BLOCK_SWAP);
   num_slots = block_size(swap_dev) / sectors_per_page;
+  slot_usage = bitmap_create(num_slots);
+}
+
+/* Deinitialise the swap system */
+void swap_destroy(void)
+{
+  bitmap_destroy(slot_usage);
+}
+
+/* Initialise a swap table */
+void swap_table_init(struct hash *table)
+{
+  hash_init(table, &swap_table_hash_func, &swap_table_less_func, NULL);
+}
+
+/* Destroy a swap table */
+void swap_table_destroy(struct hash *table)
+{
+  //TODO might need to free swap table entries as an action func?
+  hash_destroy(table, NULL);
+}
+
+/* Use vaddr as the key, and just call hash_bytes on it */
+unsigned swap_table_hash_func(const struct hash_elem *elem, void *aux UNUSED)
+{
+  struct swap_table_entry *entry
+      = hash_entry(elem, struct swap_table_entry, elem);
+  return hash_bytes(&entry->vaddr, sizeof(void *));
+}
+
+/* Use vaddr as the key and just compare those */
+bool swap_table_less_func(const struct hash_elem *a, const struct hash_elem *b,
+    void *aux UNUSED)
+{
+    struct swap_table_entry *a_entry
+        = hash_entry(a, struct swap_table_entry, elem);
+    struct swap_table_entry *b_entry
+        = hash_entry(b, struct swap_table_entry, elem);
+    return a_entry->vaddr < b_entry->vaddr;
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,6 +1,19 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include <hash.h>
+
 void swap_init(void);
+void swap_destroy(void);
+void swap_table_init(struct hash *table);
+void swap_table_destroy(struct hash *table);
+
+typedef size_t swap_index_t;
+
+struct swap_table_entry {
+  void *vaddr;            /* The start of the page that this swap block holds */
+  swap_index_t index;     /* The index of the swap block */
+  struct hash_elem elem;  /* Bookkeeping */
+};
 
 #endif
