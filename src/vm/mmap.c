23dd6c5f53e809cd39962f96da50717b7da671d0
Tanmay <tk915@ic.ac.uk>
2017-03-13 14:14:16 +0000

    [TK] Added hash and less functions for mmap_file_page

--- src/vm/mmap.c
+++ src/vm/mmap.c
@@ -3,7 +3,11 @@
 
 static unsigned fpt_hash_func(const struct hash_elem *elem, void *aux UNUSED);
 static bool fpt_less_func(const struct hash_elem *a, const struct hash_elem *b,
-                   void *aux UNUSED);
+                          void *aux UNUSED);
+static unsigned mfpt_hash_func(const struct hash_elem *elem, void *aux UNUSED);
+static bool mfpt_less_func(const struct hash_elem *a, const struct hash_elem *b,
+                           void *aux UNUSED);
+
 
 
 bool file_page_table_insert(struct hash* table, void* vaddr, struct file* file,
@@ -29,21 +33,38 @@ bool file_page_table_init(struct hash* file_table) {
 }
 
 bool mmap_file_page_table_init(struct hash* mmap_file_table) {
-  return hash_init(mmap_file_table, &fpt_hash_func, &fpt_less_func, NULL);
+  return hash_init(mmap_file_table, &mfpt_hash_func, &mfpt_less_func, NULL);
 }
 
 /* function for hashing the page pointer in a supplementary page table entry */
-unsigned fpt_hash_func(const struct hash_elem *elem, void *aux UNUSED)
+static unsigned fpt_hash_func(const struct hash_elem *elem, void *aux UNUSED)
 {
   struct file_page *entry = hash_entry(elem, struct file_page, hash_elem);
   return hash_bytes(&entry->vaddr, sizeof(entry->vaddr));
 }
 
 /* function for comparing the addresses of two supplemtary page table entries */
-bool fpt_less_func(const struct hash_elem *a, const struct hash_elem *b,
+static bool fpt_less_func(const struct hash_elem *a, const struct hash_elem *b,
                    void *aux UNUSED)
 {
   struct file_page *a_entry = hash_entry(a, struct file_page, hash_elem);
   struct file_page *b_entry = hash_entry(b, struct file_page, hash_elem);
   return a_entry->vaddr < b_entry->vaddr;
+}
+
+
+/* function for hashing the page pointer in a supplementary page table entry */
+static unsigned mfpt_hash_func(const struct hash_elem *elem, void *aux UNUSED)
+{
+  struct mmap_file_page* e = hash_entry(elem, struct mmap_file_page, hash_elem);
+  return hash_bytes(&e->vaddr, sizeof(e->vaddr));
+}
+
+/* function for comparing the addresses of two supplemtary page table entries */
+static bool mfpt_less_func(const struct hash_elem *a, const struct hash_elem *b,
+                   void *aux UNUSED)
+{
+  struct mmap_file_page* _a = hash_entry(a, struct mmap_file_page, hash_elem);
+  struct mmap_file_page *_b = hash_entry(b, struct mmap_file_page, hash_elem);
+  return _a->vaddr < _b->vaddr;
 }
