2bf89d2b457ab1d5c3cc3ab3098076d7c74b120d
Tanmay <tk915@ic.ac.uk>
2017-03-14 14:52:51 +0000

    [TK] Changed load_segment to store_segment in load

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -549,7 +549,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
                   read_bytes = 0;
                   zero_bytes = ROUND_UP (page_offset + phdr.p_memsz, PGSIZE);
                 }
-              if (!load_segment (file, file_page, (void *) mem_page,
+              if (!store_segment (file, file_page, (void *) mem_page,
                                  read_bytes, zero_bytes, writable))
                 goto done;
             }
